2463d8f708ce404a9edb19e17b18e7c31a3df3e6
Rio Zakirahman <rio.zakirahman@binus.ac.id>
2023-06-14 09:28:40 +0700

    add get data master

--- controller/companyController.js
+++ controller/companyController.js
@@ -28,5 +28,21 @@ const createCompany = (req, res) => {
     return res.json(data);
   });
 };
+const getCompany = (req, res) => {
+  const idcompany = req.query.idcompany;
+  let q;
+  if (idcompany) {
+    q = `SELECT company_name, company_address, phone_no, currency_code,countryName, provinceName, cityName FROM company JOIN country ON company.idcountry = country.idcountry JOIN province ON company.idprovince = province.idprovince JOIN city ON company.idcity = city.idcity WHERE company.idcompany = ${idcompany}`;
+  } else {
+    q =
+      "SELECT company_name, company_address, phone_no, currency_code,countryName, provinceName, cityName FROM company JOIN country ON company.idcountry = country.idcountry JOIN province ON company.idprovince = province.idprovince JOIN city ON company.idcity = city.idcity";
+  }
+  db.query(q, (err, data) => {
+    if (err) {
+      return res.json(err.sqlMessage);
+    }
+    return res.json(data);
+  });
+};
 
-export { createCompany };
+export { createCompany, getCompany };
--- router/companyRouter.js
+++ router/companyRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createCompany } from "../controller/companyController.js";
+import { createCompany, getCompany } from "../controller/companyController.js";
 
 const router = express.Router();
 
 router.post("/", createCompany);
+router.get("/", getCompany);
 
 export default router;
